49f71d0ae3dc078022ba4e5ba84893c608049a78
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-11-04 18:51:32 -0300

    Configuração inicial do projeto e adição do arquivo .gitignore

---
+++ secret-word/src/data/words.js
@@ -0,0 +1,12 @@
+export const wordsList = {
+    fruta: ["morango", "maçã", "banana", "laranja", "uva", "abacaxi", "kiwi"],
+    animal: ["cachorro", "gato", "elefante", "leão", "tigre", "urso", "macaco"],
+    cor: ["vermelho", "azul", "verde", "amarelo", "marrom", "cinza", "rosa"],
+    país: ["brasil", "argentina", "frança", "alemanha", "japão", "canadá", "austrália"],
+    objeto: ["mesa", "cadeira", "computador", "livro", "caneta", "telefone", "copo"],
+    roupa: ["camisa", "calça", "sapato", "vestido", "meia", "saia", "blusa"],
+    profissão: ["médico", "advogado", "engenheiro", "professor", "arquiteto", "dentista", "policial"],
+    veículo: ["carro", "moto", "bicicleta", "ônibus", "trem", "avião", "navio"],
+     comida: ["pizza", "hambúrguer", "macarrão", "sushi", "sopa", "churrasco", "salada"],
+    bebida: ["água", "suco", "refrigerante", "café", "chá", "leite", "cerveja"]
+}
