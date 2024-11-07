627635927976995574e5e181ad1a2cd195f0ec80
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-11-07 18:57:39 -0300

    Mudança nas palavras do jogo

--- secret-word/src/data/words.js
+++ secret-word/src/data/words.js
@@ -1,12 +1,12 @@
 export const wordsList = {
-    fruta: ["morango", "maçã", "banana", "laranja", "uva", "abacaxi", "kiwi"],
-    animal: ["cachorro", "gato", "elefante", "leão", "tigre", "urso", "macaco"],
-    cor: ["vermelho", "azul", "verde", "amarelo", "marrom", "cinza", "rosa"],
-    país: ["brasil", "argentina", "frança", "alemanha", "japão", "canadá", "austrália"],
+    fruta: ["morango", "melancia", "banana", "laranja", "uva", "abacaxi", "kiwi"],
+    animal: ["cachorro", "gato", "elefante", "capivara", "tigre", "urso", "macaco"],
+    cor: ["roxo", "azul", "verde", "amarelo", "marrom", "cinza", "rosa"],
+    país: ["brasil", "argentina", "chile", "alemanha", "marrocos", "inglaterra", "venezuela"],
     objeto: ["mesa", "cadeira", "computador", "livro", "caneta", "telefone", "copo"],
-    roupa: ["camisa", "calça", "sapato", "vestido", "meia", "saia", "blusa"],
-    profissão: ["médico", "advogado", "engenheiro", "professor", "arquiteto", "dentista", "policial"],
-    veículo: ["carro", "moto", "bicicleta", "ônibus", "trem", "avião", "navio"],
-    comida: ["pizza", "hambúrguer", "macarrão", "sushi", "sopa", "churrasco", "salada"],
-    bebida: ["água", "suco", "refrigerante", "café", "chá", "leite", "cerveja"]
+    roupa: ["camisa", "jeans", "sapato", "vestido", "meia", "saia", "blusa"],
+    profissão: ["pedreiro", "advogado", "pintor", "professor", "arquiteto", "dentista", "policial"],
+    veículo: ["carro", "moto", "bicicleta", "triciclo", "trem", "barco", "navio"],
+    comida: ["pizza", "arroz", "carne", "sushi", "risoto", "churrasco", "salada"],
+    bebida: ["agua", "suco", "vinho", "energetico", "vodka", "leite", "cerveja"]
 }
